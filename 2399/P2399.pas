f53623f0445d33f9222489a18fbd6e514ea9fbf6
li-sky <s-k-y-@sohu.com>
2017-05-12 21:32:13 +0800

    Add something to make it can read easier.

--- 2399/P2399.pas
+++ 2399/P2399.pas
@@ -3,7 +3,7 @@ var
   s,s1:ansistring;
   i,j,k,a:longint;  //hl:boolean;
   
-function gcd(aa,bb:longint):longint;
+function gcd(aa,bb:longint):longint;  //count gcd
 begin
   if bb<>0 then
   gcd:=gcd(bb,aa mod bb)
@@ -12,14 +12,16 @@ end;
 begin
   readln(s);
   i:=1; n:=length(s);
-  while s[i]<>'.' do inc(i);
+  while s[i]<>'.' do 
+    inc(i);  //find where the dot is.
   dec(i);
   for j:=1 to i do
     s1:=s1+s[j];
   val(s1,a);
   inc(i,2);
   k:=pos(s,'(');
-  if k=0 then begin
-
+  if k=0 then 
+  begin
+    for
   end
   else begin
