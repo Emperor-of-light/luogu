21e8b1e437bfbb18a1d618afd48ac19b666ae5b3
Emperor-of-light <1399458242@qq.com>
2017-05-12 21:07:12 +0800

    Update P2399.pas
    add others

--- 2399/P2399.pas
+++ 2399/P2399.pas
@@ -1,6 +1,26 @@
+program P2399;
+var
+  s,s1:ansistring;
+  i,j,k,a:longint;  //hl:boolean;
+  
 function gcd(aa,bb:longint):longint;
 begin
   if bb<>0 then
   gcd:=gcd(bb,aa mod bb)
   else gcd:=aa;
 end;
+begin
+  readln(s);
+  i:=1; n:=length(s);
+  while s[i]<>'.' do inc(i);
+  dec(i);
+  for j:=1 to i do
+    s1:=s1+s[j];
+  val(s1,a);
+  inc(i,2);
+  k:=pos(s,'(');
+  if k=0 then begin
+
+  end
+  else begin
+    while s[i]<>'(' do inc(i);
