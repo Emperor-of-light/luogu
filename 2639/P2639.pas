e4c096125757393d5809b822371bae75b53d5599
li-sky <s-k-y-@sohu.com>
2017-05-17 21:13:59 +0800

    Update P2639.pas

--- 2639/P2639.pas
+++ 2639/P2639.pas
@@ -1,1 +1,11 @@
-
+program 2539;
+var
+  a:array[0..500]of longint;
+  f:array[0..45000]of longint;
+  i,j,k,n,m:longint;
+begin
+  read(n,m);
+  for i:=1 to m do
+    read(a[i]);
+  for i:=1 to m
+end.
